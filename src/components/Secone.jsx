e9bcfda795be899e439b05afc59b8c977eaf6159
YuvCarl <carldrake969@gmail.com>
2022-09-22 05:10:17 +0100

    updated the secone.jsx component

--- src/components/Secone.jsx
+++ src/components/Secone.jsx
@@ -1,4 +1,8 @@
 import illustrationOne from '../assets/images/illustration-1.svg'
+import illustrationtwo from '../assets/images/illustration-2.svg'
+import icon from '../assets/images/icon-arrow.svg'
+import quotes from '../assets/images/icon-quotes.svg'
+import profilepic from '../assets/images/avatar-testimonial.jpg'
 
 export default function Secone(){
     return(
@@ -10,15 +14,40 @@ export default function Secone(){
                         you need, shear and collaborate with friends, family, and co-workers.
                     </p>
                     <form action="">
-                        <input type="email" required />
-                        <input type="button" value="Get Started" id="btn" />
+                        <input type="email" placeholder='Enter Your Email' required />
+                        <input type="submit" value="Get Started" id="btn" />
                     </form>
                 </aside>
 
                 <section>
-                    <img src={illustrationOne} alt="" />
+                    <img className='img-1' src={illustrationOne} alt="" />
                 </section>
             </main>
+
+        <div className="secone">
+        <main>
+                <aside>
+                    <h1>stay productive, wherever you are.</h1>
+                    <p>Never let location be an issue when accesig your file. Fylo has you covered for all of your file storage needs.</p>
+                    <p>Securely shear files and folders with friends, family and collleagues for live collaboration. No email attachments required!</p>
+                    <a className='a' href="../app.jsx">See how Fylo works <img src={icon} alt="" /> </a>
+                    <div className="testimonial">
+                        <img src={quotes} alt="" />
+                        <p>Fylo has improved our team productivity by an order of magnitude. Since maiking 
+                            the switch our team has become a well-oiled collaboration mechine
+                        </p>
+                        <div className="user">
+                            <img src={profilepic} className="img-3" alt="" />
+                            <h4>Kyle Burton <br/> <span>Founder and CEO Hddie</span></h4>
+                        </div>
+                    </div>
+                </aside>
+                <section>
+                    <img src={illustrationtwo} className='img-1' alt="" />
+                </section>
+            </main>
+        </div>
+           
         </div>
     )
 }
