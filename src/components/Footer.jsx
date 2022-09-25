2775c85b75d6e1b7ebde509a769fc4df33028a06
YuvCarl <carldrake969@gmail.com>
2022-09-25 11:57:48 +0100

    updated the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,12 +12,22 @@ export default function Footer(){
                 <img  src={logo} alt="" className='logo wh' />
                 <div className="w-1"><img className="wh" src={phone} alt="" /> <span>Phone: 222 222 222</span></div>
                 <div className="w-1"><img className="wh" src={email} alt="" /> <span>Example@fylo.com</span></div>
-                <div className="w-1"></div>
+
+            </div>
+            <div className="same">
                 
+                    <a href="../app.js">About</a>
+                    <a href="../app.js">Terms</a>
+                    <a href="../app.js">Checks</a>
                 
             </div>
-            <div className="same"></div>
-            <div className="same"></div>
+            
+                 <div className='same'>
+                    <a href="../app.js">About</a>
+                    <a href="../app.js">Terms</a>
+                    <a href="../app.js">Checks</a>
+                
+            </div>
         </div>
     )
 }
